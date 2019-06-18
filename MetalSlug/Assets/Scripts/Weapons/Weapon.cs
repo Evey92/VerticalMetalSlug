9ca770bb9447452b01bb5ba4dc8fabb20aa92b0d
Ivan <evey9207@gmail.com>
2019-06-17 20:48:34 -0500

    Created Basic player movements with a player state machine

---
+++ MetalSlug/Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+  /// <summary>
+  /// Used to instantiate a m_bullet
+  /// </summary>
+  public abstract void Shoot(); 
+
+  public GameObject m_bulletSpawn;
+  public GameObject m_bullet;
+  
+  protected float m_firePower;
+  protected float m_fireRate;
+  protected float m_range;
+  protected int m_ammo;
+
+}
