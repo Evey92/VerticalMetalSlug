136e9f9023f9e5718abce55c859f96d6c9737c19
Virato913 <idv16c.jvilla@uartesdigitales.edu.mx>
2019-06-17 19:07:44 -0500

    + Created and added most enemy states

---
+++ MetalSlug/Assets/Scripts/EnemyIdle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyIdle : State<Enemy>
+{
+  public EnemyIdle(StateMachine<Enemy> stateMachine)
+  : base(stateMachine) { }
+
+  public override void OnStateEnter(Enemy enemy)
+  {
+
+  }
+
+  public override void OnStatePreUpdate(Enemy enemy)
+  {
+
+  }
+
+  public override void OnStateUpdate(Enemy enemy)
+  {
+
+  }
+
+  public override void OnStateExit(Enemy enemy)
+  {
+
+  }
+}
---
+++ MetalSlug/Assets/Scripts/EnemyJumping.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyJumping : State<Enemy>
+{
+  public EnemyJumping(StateMachine<Enemy> stateMachine)
+  : base(stateMachine) { }
+
+  public override void OnStateEnter(Enemy enemy)
+  {
+
+  }
+
+  public override void OnStatePreUpdate(Enemy enemy)
+  {
+
+  }
+
+  public override void OnStateUpdate(Enemy enemy)
+  {
+
+  }
+
+  public override void OnStateExit(Enemy enemy)
+  {
+
+  }
+}
