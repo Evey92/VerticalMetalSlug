4f271daf2c382a8702ebe95f6f4b12300089e048
Evey92 <evey92@gmail.com>
2019-06-23 08:45:27 -0500

    Made a somewhat playable level with just 2 enemies that actually die. No, they don't do anything.

---
+++ MetalSlug/Assets/Scripts/Items/AmmoFallState.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoFallState : State<AmmoItem>
+{
+  public AmmoFallState(StateMachine<AmmoItem> stateMachine)
+: base(stateMachine) { }
+
+  public override void OnStateEnter(AmmoItem ammo)
+  {
+    Debug.Log("Weapon fall state");
+    ammo.FallSpeed = 0;
+  }
+
+  public override void OnStatePreUpdate(AmmoItem ammo)
+  {
+    if (ammo.IsGrounded)
+    {
+      m_StateMachine.ToState(ammo.ammoIdleState, ammo);
+    }
+  }
+
+  public override void OnStateUpdate(AmmoItem ammo)
+  {
+    ammo.Fall();
+
+  }
+
+  public override void OnStateExit(AmmoItem ammo)
+  {
+    ammo.FallSpeed = 0;
+
+  }
+}
---
+++ MetalSlug/Assets/Scripts/Items/AmmoPickedUpState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickedUpState : State<AmmoItem>
+{
+  public AmmoPickedUpState(StateMachine<AmmoItem> stateMachine)
+: base(stateMachine) { }
+
+  public override void OnStateEnter(AmmoItem ammo)
+  {
+    Debug.Log("Item picked up");
+    ammo.m_audioSource.PlayOneShot(ammo.m_pickUpClip);
+  }
+
+  public override void OnStatePreUpdate(AmmoItem ammo)
+  {
+    m_StateMachine.ToState(ammo.ammoDestroyedstate, ammo);
+
+  }
+
+  public override void OnStateUpdate(AmmoItem ammo)
+  {
+
+  }
+
+  public override void OnStateExit(AmmoItem ammo)
+  {
+
+  }
+}
